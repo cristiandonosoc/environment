a3b6ec6b282694fe8f15c3bc2c6686282067a625
Cristián Donoso <donosoc@google.com>
2018-06-26 18:36:49 -0700

    Initial commit

---
+++ config.fish
@@ -0,0 +1,12 @@
+# aliases
+alias ll "ls -lha"
+alias grep "grep --color"
+alias vim "nvim"
+
+set -g EDITOR nvim
+
+# Bob-the-fish
+set -g theme_newline_cursor yes
+set -g theme_display_git yes
+set -g theme_show_exit_status yes
+set -g fish_prompt_pwd_dir_length 0
---
+++ initial_fish.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+# Install fisher
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+
+# Install packages
+fisher laughedelic/pisces
+fisher omf/theme-bobthefish
+
